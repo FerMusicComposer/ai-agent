7a26331ad7a4d1f0d052390d187fa9c4c7ee7c80
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-06-26 00:01:42 +0200

    added file listing functionality with system prompt and function declarations

--- main.py
+++ main.py
@@ -21,13 +21,39 @@ if not user_prompt:
     print("A prompt must be provided.")
     sys.exit(1)
 
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+available_functions = types.Tool(
+    function_declarations=[schema_get_files_info]
+)
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
 ]
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
 )
 
 if verbose:
@@ -35,4 +61,8 @@ if verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(f"Response:\n{response.text}")
+if response.function_calls is not None:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+else:
+    print(f"Response:\n{response.text}")
