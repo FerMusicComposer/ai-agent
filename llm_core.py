c95806745c3ad5326192c2eac518850f2855ad1d
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-06-27 22:34:48 +0200

    added function call capability and refactored main

---
+++ llm_core.py
@@ -0,0 +1,92 @@
+import sys
+from google.genai import types
+
+model = "gemini-2.0-flash-001"
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read the contents of a file
+- Write to a file (if it does not exist, create it)
+- Run a Python file
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+**Important Note:** If the user asks to "execute the calculator app's tests" or similar, assume the Python file path is 'tests.py' within the calculator's working directory. For other calculator-related tasks, such as running its main application, assume 'main.py'.
+"""
+
+user_prompt = sys.argv[1]
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the contents of a file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes or overwirtes the contents of a file, and if the file does not exist, creates it, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a Python file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the Python file to run, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_write_file,
+        schema_run_python_file
+    ]
+)
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+import llm_core as core
+from functions.call_function import call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -11,58 +13,28 @@ if len(sys.argv) < 2:
     print("A prompt must be provided.")
     sys.exit(1)
 
-user_prompt = sys.argv[1]
-
 verbose = False
 if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
     verbose = True
 
-if not user_prompt:
+if not core.user_prompt:
     print("A prompt must be provided.")
     sys.exit(1)
 
-system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-schema_get_files_info = types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=types.Schema(
-        type=types.Type.OBJECT,
-        properties={
-            "directory": types.Schema(
-                type=types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
-)
-available_functions = types.Tool(
-    function_declarations=[schema_get_files_info]
-)
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
-
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+    model=core.model,
+    contents=core.messages,
+    config=types.GenerateContentConfig(tools=[core.available_functions], system_instruction=core.system_prompt)
 )
 
 if verbose:
-    print(f"User prompt:\n{user_prompt}")
+    print(f"User prompt:\n{core.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if response.function_calls is not None:
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        print(f"-> {function_call_result.parts[0].function_response.response}")
 else:
     print(f"Response:\n{response.text}")
